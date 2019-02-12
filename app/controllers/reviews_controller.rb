304c8e4a2723f8e6b9e0005d2467089b9b738d88
Albrecht Bernstorff <albrecht.bernstorff@web.de>
2019-02-12 13:31:09 +0200

    migration, changed phone_number type from integer to string

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,13 @@
 class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    raise
+    @review = Review.new(params_review)
+    @review.restaurant = Restaurant.find(params[:restaurant_id])
+    @review.save
+  end
 end
